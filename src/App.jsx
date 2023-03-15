dc9e19c9fee63efb11dbafee56db97801b21633b
Felipe de Jesus <thugfa001@gmail.com>
2023-03-15 09:28:57 -0600

    Listas de Hobbies Frontend

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  const nombre = "Felipe de Jesus Lopez Ortega";
+  return (
+    <div>
+      <h3>Lista de Hobbies del Alumno {nombre}</h3>
+      <ol>
+        <li>Ver videos de Youtube</li>
+        <li>Jugar Video Juegos</li>
+        <li>Leer Mangas y libros</li>
+        <li>Ver Peliculas</li>
+        <li>Ver Anime</li>
+      </ol>
+    </div>
+  )
+}
+
+export default App
